892ee3558ed693ce1299968c6ddea1d2fea583e9
yeabi-tech <yeabisolo@gmail.com>
2025-05-27 02:21:10 +0200

    Add files via upload

---
+++ BMI_trial1.py
@@ -0,0 +1,70 @@
+# name = input("Enter you name: ")
+
+# weight = int(input("Enter your weight in pounds: "))
+
+# height = int(input("Enter your height in inches: "))
+
+# BMI = (weight * 703) / (height * height)
+
+# print(BMI)
+
+# if BMI>0:
+#     if(BMI<18.5):
+#         print(name +", you are underwight.")
+#     elif (BMI<=24.9):
+#         print(name +", you are normal weight.")
+#     elif (BMI<29.9):
+#         print(name +", you are overweight. You need to exercise more and stop sitting and writing so many python tutorials.")
+#     elif (BMI<34.9):
+#         print(name +", you are obese.")
+#     elif (BMI<39.9):
+#         print(name +", you are severely obese.")
+#     else:
+#         print(name +", you are morbidly obese.")
+# else:
+#     print("Enter valid input")
+
+
+ 
+ 
+ 
+# #BMI = (weight in pounds x 703) / (height in inches x height in inches)
+# print(weight)
+
+# if BMI>0:
+#     if(BMI<18.5):
+#         print(name +", you are underwight.")
+#     elif (BMI<=24.9):
+#         print(name +", you are normal weight.")
+#     elif (BMI<29.9):
+#         print(name +", you are overweight.")
+#     elif (BMI<34.9):
+#         print(name +", you are obese.")
+#     elif (BMI<39.9):
+#         print(name +", you are severely obese.")
+#     else:
+#         print(name +", you are morbidly obese.")
+# else:
+#     print("Enter valid input")
+
+name=input("enter your name: ")
+Weight =int(input("enter your weight in kg: "))
+height=float(input("enter height in meters: "))
+BMI=(Weight )/(height)
+if BMI>0:
+    if(BMI<40):
+        print(name+",you are skinny.you need to gain muscles")
+    elif(BMI<60):
+        print(name+",you are thin")
+    elif(BMI<80):
+        print(name+",you are midsize")
+    else:
+        print(name+",just input facts")
+
+
+print(f"Here is your BMI {BMI}")
+
+
+
+
+
